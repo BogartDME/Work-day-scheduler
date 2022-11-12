dfe48e4cab166555be86bf55a99c5a3c6648e865
BogartDME <edwin.pietrowski@gmail.com>
2022-11-12 18:31:35 -0500

    changed id for each time in the divs and in js created function to save in local storage and created function to current time and reflect on the schedule

--- script.js
+++ script.js
@@ -1,54 +1,40 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    var today = dayjs();
-
-    $('#currentDay').text(today.format('dddd MMMM DD, YYYY'));
-
-    $(".saveBtn").on("click", function () {
-      
-      var = time $(this).parent.attr("id"); 
-      var userInput= $(this).siblings(".user-input").val();
-
-      //saves the user input into local storage
-      localStorage.setItem(time, userInput);
-      console.log(userInput, time)
-    })
-    
-
-
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-    
-    function hourTracking () {
-    //tracks just what the current hours is
-      var hrs= dayjs().hour(); 
-    }
- 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+$(document).ready(function () {
+  // Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
   //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var today = dayjs();
+  var hr = dayjs().hour();
+    console.log("currently the hour is " + hr);
+    
+    $("#currentDay").text(today.format("MMMM DD, dddd"));
+    
+    $(".time-block").each(function(){
+    var apptBlock= $(this).attr("id");
+      if (apptBlock<hr) $(this).addClass("past").removeClass("present").removeClass("future")
+        else if (apptBlock==hr) $(this).addClass("present").removeClass("past").removeClass("future")
+        else $(this).addClass("future").removeClass("past").removeClass("present")
 });
+
+
+
+    // Listener for click event on the save buttons
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var userInput = $(this).siblings(".user-input").val();
+    localStorage.setItem(time, userInput);
+    console.log(time, userInput);
+  });
+  
+  
+
+  
+});
+  
+  
