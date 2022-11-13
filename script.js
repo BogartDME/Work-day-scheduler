f7d7ade1949e652e97f09d9edab7415252798394
BogartDME <edwin.pietrowski@gmail.com>
2022-11-12 21:09:37 -0500

    added methods to call back local storage

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//war
 $(document).ready(function () {
-  // Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
   var today = dayjs();
   var hr = dayjs().hour();
     console.log("currently the hour is " + hr);
@@ -25,6 +17,7 @@ $(document).ready(function () {
 
 
     // Listener for click event on the save buttons
+    //created variables to store user input into local storage
   $(".saveBtn").on("click", function () {
     var time = $(this).parent().attr("id");
     var userInput = $(this).siblings(".user-input").val();
@@ -32,6 +25,16 @@ $(document).ready(function () {
     console.log(time, userInput);
   });
   
+  //here is the method to call back what was stored in the local storage
+  $("#9 .user-input").val(localStorage.getItem("9"));
+  $("#10 .user-input").val(localStorage.getItem("10"));
+  $("#11 .user-input").val(localStorage.getItem("11"));
+  $("#12 .user-input").val(localStorage.getItem("12"));
+  $("#13 .user-input").val(localStorage.getItem("13"));
+  $("#14 .user-input").val(localStorage.getItem("14"));
+  $("#15 .user-input").val(localStorage.getItem("15"));
+  $("#16 .user-input").val(localStorage.getItem("16"));
+  $("#17 .user-input").val(localStorage.getItem("17"));
   
 
   
